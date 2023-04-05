a9458e91c9f0a03c17647bad2a6b658b8488603f
Eric <ericchiuboy@gmail.com>
2023-04-05 23:12:42 +0800

    20230405L

---
+++ scripts/cityscapes2foggy/train_resume.sh
@@ -0,0 +1,3 @@
+# SAPNetV2
+
+python tools/train_net.py --num-gpus 1 --config-file "configs//sapnetV2_R_50_C4.yaml" --resume OUTPUT_DIR "outputs/" 
--- scripts/cityscapes2foggy/train_sapv2.sh
+++ scripts/cityscapes2foggy/train_sapv2.sh
@@ -1,4 +1,5 @@
-source_model_path="pretrained/cityscapes-baseline/model_0019999_MAP_32_21_T002_V002.pth"
+source_model_path="pretrained/cityscapes-baseline-source-only/model_0019999_MAP_32_21_T002_V002.pth"
+
 # SAPNetV2
 weight_DA=0.6
 weight_entropy=1.0
@@ -9,4 +10,3 @@ python tools/train_net.py --config-file "configs/cityscapes2foggy/sapnetV2_R_50_
 MODEL.DA_HEAD.LOSS_WEIGHT ${weight_DA} MODEL.DA_HEAD.TARGET_ENT_LOSS_WEIGHT ${weight_entropy} MODEL.DA_HEAD.TARGET_DIV_LOSS_WEIGHT -${weight_diversity} \
 MODEL.WEIGHTS ${source_model_path}
 
-# --resume OUTPUT_DIR "outputs/output-city2foggy-sapnetV2-l0.6-e1.0-d0.2-23-03-09_00-20/" \
