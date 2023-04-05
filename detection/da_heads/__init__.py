9b825bab4a31f3e1d4f6dc04e41a323d9562cf1f
Eric <ericchiuboy@gmail.com>
2023-04-05 17:07:01 +0800

    20230405

--- detection/da_heads/__init__.py
+++ detection/da_heads/__init__.py
@@ -2,4 +2,5 @@ from .sapnet import SAPNet
 from .sapnetMSCAM import SAPNetMSCAM
 from .sapnetiAFF import SAPNetiAFF
 from .sapnetAFF import SAPNetAFF
+from .sapnetMSCAM2 import SAPNetMSCAM2
 from .build import build_DAHead
