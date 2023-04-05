480b41ab928f3629f72a86f36a8c6218ef27e75d
Eric <ericchiuboy@gmail.com>
2023-04-05 22:38:46 +0800

    20230405L

---
+++ scripts/sim10k2cityscapes/train_resume.sh
@@ -0,0 +1,4 @@
+# SAPNetV2
+
+python tools/train_net.py --num-gpus 1 --config-file "configs/sim10k2cityscapes/sapnetV2_R_50_C4.yaml" --resume OUTPUT_DIR "outputs/output-sim10k2city-sapnetV2-l0.1-e0.8-d0.3-23-04-05_16-41" 
+
